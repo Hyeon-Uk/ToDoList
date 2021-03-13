2ba37ebceb5ce36f70c37bcfd07ef02b622064ab
Hyeon-Uk <rlagusdnr120@gmail.com>
2021-03-13 21:45:58 +0900

    to do list

---
+++ javascript/todo.js
@@ -0,0 +1,9 @@
+const toDoForm = document.querySelector('.js-toDoForm');
+const toDoInput=toDoForm.querySelector("input");
+const toDoList=docuemt.querySelector('.js-toDoList');
+
+
+function init(){
+
+}
+init();
