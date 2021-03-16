f31f121d57aed142ad5d6ae71d9ad6a6dd7911c7
Hyeon-Uk <43038815+Hyeon-Uk@users.noreply.github.com>
2021-03-17 01:30:50 +0900

    Update bg.js

--- javascript/bg.js
+++ javascript/bg.js
@@ -2,7 +2,7 @@ const body=document.querySelector('body');
 
 function setBackground(){
     fetch(
-        `https://source.unsplash.com/random/1920`
+        `https://source.unsplash.com/random/1920x1080`
     ).then(function(response){
         const image=new Image();
         image.src=response.url;
