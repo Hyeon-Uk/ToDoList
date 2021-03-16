3f7910a1247880f89d2e2fdb25c0351470046da2
Hyeon-Uk <rlagusdnr120@gmail.com>
2021-03-16 20:52:03 +0900

    update"

--- javascript/bg.js
+++ javascript/bg.js
@@ -1,27 +1,18 @@
 const body=document.querySelector('body');
 
-const IMG_NUMBER=5;
-
-function getRandom(){
-    const number=Math.floor(Math.random()*IMG_NUMBER)+1;
-    return number;
-}
-
-function setBackground(randomNumber){
-    const image=new Image();
-    image.src=`./images/${randomNumber}.jpg`;
-    image.classList.add("bg");
-
-    body.prepend(image);
-    // body.style.backgroundImage=`url('./images/${randomNumber}.jpg')`;
-    // body.style.backgroundSize="cover";
-    // body.style.backgroundRepeat="no-repeat";
-    // body.style.backgroundPosition="center";
+function setBackground(){
+    fetch(
+        `https://source.unsplash.com/random`
+    ).then(function(response){
+        const image=new Image();
+        image.src=response.url;
+        image.classList.add("bg");
+        body.prepend(image);
+    })
 }
 
 function init(){
-    const randomNumber=getRandom();
-    setBackground(randomNumber);
+    setBackground();
 }
 
 init();
--- javascript/greeting.js
+++ javascript/greeting.js
@@ -1,14 +1,27 @@
 const form = document.querySelector('.js-form');
 const input = form.querySelector('input');
 const greeting=document.querySelector(".js-greetings");
-const logoutBtn=document.querySelector("#logoutBtn");
+const logoutBtn=document.querySelector(".renameBtn");
+const showList=document.querySelector(".js-toDoList");
+const showForm=document.querySelector(".js-toDoForm");
+
 const USER_LS="currentUser";
 const SHOWING_CN="showing";
+
+function changeName(){
+    localStorage.removeItem(USER_LS);
+    location.reload();
+}
 
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText=`Hello ${text}`;
+
+    logoutBtn.addEventListener("click",changeName);
+    logoutBtn.classList.remove("disappear");
+    showList.classList.remove("disappear");
+    showForm.classList.remove("disappear");
 }
 
 function saveName(text){
@@ -24,6 +37,9 @@ function handleSubmit(event){
 function askForName(){
     form.classList.add(SHOWING_CN);
     form.addEventListener("submit",handleSubmit);
+    logoutBtn.classList.add("disappear");
+    showList.classList.add("disappear");
+    showForm.classList.add("disappear");
 }
 
 function loadName(){
