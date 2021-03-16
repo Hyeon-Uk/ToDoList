327faca4c916395ecf6f232e0cd6248ca24c292d
Hyeon-Uk <rlagusdnr120@gmail.com>
2021-03-17 01:27:40 +0900

    bg image size update

--- javascript/bg.js
+++ javascript/bg.js
@@ -2,10 +2,11 @@ const body=document.querySelector('body');
 
 function setBackground(){
     fetch(
-        `https://source.unsplash.com/random`
+        `https://source.unsplash.com/random/1920x1080`
     ).then(function(response){
         const image=new Image();
         image.src=response.url;
+        console.log(response);
         image.classList.add("bg");
         body.prepend(image);
     })
