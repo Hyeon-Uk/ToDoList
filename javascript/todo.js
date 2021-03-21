729c07bf6ba91b14291cdd732d6691432df34e2b
Hyeon-Uk <rlagusdnr120@gmail.com>
2021-03-21 22:20:25 +0900

    finishedlist localstorage set

--- javascript/todo.js
+++ javascript/todo.js
@@ -1,11 +1,27 @@
 const toDoForm = document.querySelector('.js-toDoForm');
 const toDoInput=toDoForm.querySelector("input");
 const toDoList=document.querySelector('.js-toDoList');
-
 const TODOS_LS="toDos";
+const FINISHED_LS="finishedToDos";
 
 let toDos=[];
-let number=1;
+
+let finished=[];
+
+function saveFinished(){
+    localStorage.setItem(FINISHED_LS,JSON.stringify(finished));
+}
+
+function clearToDo(event){
+    const btn=event.target;
+    const li=btn.parentNode;
+    const inner=li.innerText;
+    const text=inner.slice(0,inner.length-2);
+
+    finished.push(text);
+    deleteToDo(event);
+    saveFinished();
+}
 
 function deleteToDo(event){
     const btn=event.target;
@@ -31,13 +47,17 @@ function saveToDos(){
 function paintToDo(text){
     const li=document.createElement("li");
     const delBtn=document.createElement("button");
+    const successBtn=document.createElement("button");
     const span=document.createElement("div");
-    const newId=number++;
+    const newId=Date.now();
     delBtn.innerText="X";
+    successBtn.innerText="V";
     delBtn.addEventListener("click",deleteToDo);
+    successBtn.addEventListener("click",clearToDo);
     span.innerText=text;
     li.appendChild(span);
     li.appendChild(delBtn);
+    li.appendChild(successBtn);
     li.id=newId;
     toDoList.appendChild(li);
     const toDosObj={
