220be25b427e607750a20258ca94f8673711a74a
Hyeon-Uk <rlagusdnr120@gmail.com>
2021-03-26 23:54:42 +0900

    domain

--- javascript/search.js
+++ javascript/search.js
@@ -1,8 +1,26 @@
-const searchBar=document.querySelector("#google-searchbar");
+const searchBar = document.querySelector("#google-searchbar");
+
+function domain(str) {
+    let regEx = /^(?:www\.)?(.*?):\/\//gim;
+    let url = str;
+    let path = url.replace(regEx, "");
+    console.log("path = " + path);
+    //removes domain extracts route
+    let regEx2 = /^(.*?\/)/;
+    if (path.match(regEx2)) {
+        let route = "/" + path.replace(regEx2, "");
+        console.log("route", route);
+        //extracts domain
+        url = path.match(regEx2);
+        let domainUrl = url[0].replace("/", "");
+        console.log("domainUrl = ", domainUrl);
+    }
+}
 
 //searchForm==greeting.js에 선언
-searchForm.addEventListener("submit",function(event){
+searchForm.addEventListener("submit", function (event) {
     event.preventDefault();
-    location.href=`https://www.google.co.kr/search?q=${searchBar.value}`;
-    searchBar.value="";
+    domain(searchBar.value);
+    // location.href = `https://www.google.co.kr/search?q=${searchBar.value}`;
+    searchBar.value = "";
 });
