ee0087923907d2f06738c4c272efbe44a48c336a
Hyeon-Uk <rlagusdnr120@gmail.com>
2021-03-27 00:17:23 +0900

    url page move update

--- javascript/search.js
+++ javascript/search.js
@@ -1,26 +1,23 @@
 const searchBar = document.querySelector("#google-searchbar");
-
-function domain(str) {
-    let regEx = /^(?:www\.)?(.*?):\/\//gim;
-    let url = str;
-    let path = url.replace(regEx, "");
-    console.log("path = " + path);
-    //removes domain extracts route
-    let regEx2 = /^(.*?\/)/;
-    if (path.match(regEx2)) {
-        let route = "/" + path.replace(regEx2, "");
-        console.log("route", route);
-        //extracts domain
-        url = path.match(regEx2);
-        let domainUrl = url[0].replace("/", "");
-        console.log("domainUrl = ", domainUrl);
+function checkUrl(str) {
+    //url 유효성 검사
+    let regex =  /^(http\:\/\/)?((\w+)[.])+(asia|biz|cc|cn|com|de|eu|in|info|jobs|jp|kr|mobi|mx|name|net|nz|org|travel|tv|tw|uk|us)(\/(\w*))*$/i;
+    //올바른 url이 맞다면 해당 url로 이동
+    if (regex.test(str)) {
+        //location.href =str;
+        if(str.indexOf("http://")==-1&&str.indexOf("https://")==-1){
+            str=`http://${str}`;
+        }
+        location.href=str;
+    }
+    else{
+        location.href = `https://www.google.co.kr/search?q=${str}`;
     }
 }
 
 //searchForm==greeting.js에 선언
 searchForm.addEventListener("submit", function (event) {
     event.preventDefault();
-    domain(searchBar.value);
-    // location.href = `https://www.google.co.kr/search?q=${searchBar.value}`;
+    checkUrl(searchBar.value);
     searchBar.value = "";
 });
